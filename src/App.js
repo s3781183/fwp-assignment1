371c7f3b5c89e79485bac97523cb9408918d2dbf
Ruchelle <s3781183@student.rmit.edu.au>
2022-08-03 18:46:05 +1000

    added sign up route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignUp from "./components/Signup";
 
 function App() {
+
   return (
-    <div className="App">
-        <h1> landing page
-          
-        </h1>
+    <div className="d-flex flex-column min-vh-100">
+      <Router>
+          <div>
+            <Routes>
+              <Route path="/signup" element={<SignUp/>} />
+            </Routes>
+          </div>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
